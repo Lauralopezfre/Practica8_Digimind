d7a998a2bdcbcdb08a19e520d6f171d0465c39e1
Lauralopezfre <15325051520023@cetis108.edu.mx>
2021-03-27 22:49:06 -0700

    Falta probar

--- app/src/main/java/lopez/laura/mydigimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/lopez/laura/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -1,18 +1,28 @@
 package lopez.laura.mydigimind.ui.dashboard
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import lopez.laura.mydigimind.R
+import lopez.laura.mydigimind.ui.Task
+import lopez.laura.mydigimind.ui.home.HomeFragment
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,10 +32,35 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        btn_time.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                btn_time.text = SimpleDateFormat("HH:mm")
+                        .format(cal.time)
+            }
+
+            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE),true).show()
+            }
+        btn_save.setOnClickListener {
+            var title = et_task.text.toString()
+            var time = btn_time.text.toString()
+            var days = ArrayList<String>()
+
+            if(checkMonday.isChecked)days.add("Monday")
+            if(checkTuesday.isChecked)days.add("Tuesday")
+            if(checkWednesday.isChecked)days.add("Wednesday")
+            if(checkThursday.isChecked)days.add("Thursday")
+            if(checkFriday.isChecked)days.add("Friday")
+            if(checkSaturday.isChecked)days.add("Saturday")
+            if(checkSunday.isChecked)days.add("Sunday")
+
+            var task = Task(title, days, time)
+            HomeFragment.tasks.add(task)
+            Toast.makeText(root.context, "new task added", Toast.LENGTH_SHORT).show()
+        }
+
         return root
     }
 }
--- app/src/main/java/lopez/laura/mydigimind/ui/home/HomeFragment.kt
+++ app/src/main/java/lopez/laura/mydigimind/ui/home/HomeFragment.kt
@@ -1,19 +1,29 @@
 package lopez.laura.mydigimind.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.task_view.view.*
 import lopez.laura.mydigimind.R
+import lopez.laura.mydigimind.ui.Task
 
 class HomeFragment : Fragment() {
-
+    var tasks = ArrayList<Task>()
+    private var adaptador: AdaptadorTareas? = null
     private lateinit var homeViewModel: HomeViewModel
 
+    companion object{
+        var tasks = ArrayList<Task>()
+        var first = true
+    }
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -22,8 +32,60 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-        })
+
+        if(first){
+            fillTask()
+            first = false
+        }
+
+
+        adaptador = AdaptadorTareas(root.context, tasks)
+        gridView.adapter = adaptador
         return root
+    }
+
+    fun fillTask(){
+        tasks.add(Task("Practice 1", arrayListOf("Tuesday"), "17:30"))
+        tasks.add(Task("Practice 2", arrayListOf("Monday", "Sunday"), "17:30"))
+        tasks.add(Task("Practice 3", arrayListOf("Wednesday"), "14:00"))
+        tasks.add(Task("Practice 4", arrayListOf("Saturday"), "11:00"))
+        tasks.add(Task("Practice 5", arrayListOf("Friday"), "13:00"))
+        tasks.add(Task("Practice 6", arrayListOf("Thursday"), "10:40"))
+        tasks.add(Task("Practice 7", arrayListOf("Monday"), "12:00"))
+    }
+
+    private class AdaptadorTareas: BaseAdapter {
+        var tasks = ArrayList<Task>()
+        var contexto: Context? = null
+
+        constructor(contexto: Context, tasks: ArrayList<Task>){
+            this.contexto = contexto
+            this.tasks = tasks
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var task = tasks[position]
+            var inflator = LayoutInflater.from(contexto)
+            var vista = inflator.inflate(R.layout.task_view, null)
+
+            vista.tv_title.setText(task.title)
+            vista.tv_time.setText(task.time)
+            vista.tv_days.setText(task.days.toString())
+
+            return vista
+        }
+
+        override fun getItem(position: Int): Any {
+            return tasks[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return tasks.size
+        }
+
     }
 }
